9d77d3996d1d92fb072758d2be9b528396be7dd0
GitHub Action <action@github.com>
2023-12-17 21:45:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/andrelmv/kotlinconststringinterpolationplugin/MyBundle.kt
+++ src/main/kotlin/com/github/andrelmv/kotlinconststringinterpolationplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.andrelmv.kotlinconststringinterpolationplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/andrelmv/kotlinconststringinterpolationplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/andrelmv/kotlinconststringinterpolationplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.andrelmv.kotlinconststringinterpolationplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
